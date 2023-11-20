e225399670d06f53ea63926258a63a3de8e2734b
Miguel Quispe <miguelqn500@gmail.com>
2023-11-19 21:21:00 -0300

    Creacion del proyecto

---
+++ todo-list/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>ToDo List</h1>
+      <div>
+      </div>
+    </>
+  )
+}
+
+export default App
